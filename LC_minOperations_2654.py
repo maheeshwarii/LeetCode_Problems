93b9926e41b6ff737fa11ccc58e86c8acec010db
maheeshwarii <maheshwarikothagadi@gmail.com>
2025-11-13 00:01:19 +0530

    Create LC_minOperations_2654.py

---
+++ LC_minOperations_2654.py
@@ -0,0 +1,16 @@
+def minOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+        ones = nums.count(1)
+        if ones > 0:
+            return n - ones
+
+        # the minimum operations to make the shortest subarray with a gcd == 1
+        minOps = math.inf
+
+        for i, g in enumerate(nums):
+            for j in range(i + 1, n):
+                g = math.gcd(g, nums[j])
+                if g == 1:   # gcd(nums[i..j]:== 1
+                    minOps = min(minOps, j - i)
+                    break
+        return -1 if minOps == math.inf else minOps + n - 1
