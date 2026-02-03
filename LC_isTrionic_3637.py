5c50eed666e63c39e13ab87f78d0e3f9a1eca4e5
Maheshwari <maheshwarikothagadi@gmail.com>
2026-02-03 22:44:19 +0530

    Check if array is trionic

---
+++ LC_isTrionic_3637.py
@@ -0,0 +1,21 @@
+def isTrionic(nums):
+    n = len(nums)
+    p = 0
+    while p < n - 2 and nums[p] < nums[p + 1]:
+        p += 1
+    if p == 0 :
+        return False
+    q = p
+    while q < n - 1 and nums[q] > nums[q + 1]:
+        q += 1
+    if p == q or q == n - 1:
+        return False
+    while q < n - 1 and nums[q] < nums[q + 1]:
+        q += 1
+    return q == n - 1
+
+#main
+nums = [1,3,5,4,2,6]
+print(isTrionic(nums))
+nums = [2,1,3]
+print(isTrionic(nums))
