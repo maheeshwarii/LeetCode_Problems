636deb9af1f66da5ee8e24c0fadb6199749e253c
Maheshwari <maheshwarikothagadi@gmail.com>
2026-02-13 21:13:36 +0530

    Longest palindrome

---
+++ LC_longestPalindrome_5.py
@@ -0,0 +1,16 @@
+def longestPalindrome(s):
+    n = len(s)
+    f = [[True] * n for _ in range(n)]
+    k, mx = 0, 1
+    for i in range(n - 2, -1, -1):
+        for j in range(i + 1, n):
+            f[i][j] = False
+            if s[i] == s[j]:
+                f[i][j] = f[i + 1][j - 1]
+                if f[i][j] and mx < j - i + 1:
+                    k, mx = i, j - i + 1
+    return s[k : k + mx] 
+
+#main
+s = "babad"
+print(longestPalindrome(s))
