10d27807c0e8d8aa0cc8ebe71219250b10c0f891
Maheshwari <maheshwarikothagadi@gmail.com>
2026-03-09 21:07:18 +0530

    Stable Binary Arrays I

---
+++ numberOfStableArrays_3129.py
@@ -0,0 +1,21 @@
+def numberOfStableArrays(zero, one, limit):
+    MOD = 10**9 + 7
+    dp = [[[0] * 2 for _ in range(one + 1)] for _ in range(zero + 1)]
+
+    for i in range(min(zero, limit) + 1):
+        dp[i][0][0] = 1
+    for j in range(min(one, limit) + 1):
+        dp[0][j][1] = 1
+    for i in range(1, zero + 1):
+        for j in range(1, one + 1):
+            dp[i][j][0] = (dp[i - 1][j][0] + dp[i - 1][j][1] -
+                (dp[i - limit - 1][j][1] if i - limit >= 1 else 0) + MOD) % MOD
+            dp[i][j][1] = (dp[i][j - 1][0] + dp[i][j - 1][1] -
+                (dp[i][j - limit - 1][0] if j - limit >= 1 else 0) + MOD) % MOD
+    return (dp[zero][one][0] + dp[zero][one][1]) % MOD
+
+#main
+zero = 1
+one = 1
+limit = 2
+print(numberOfStableArrays(zero, one, limit)) 
