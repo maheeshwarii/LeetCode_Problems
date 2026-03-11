c22a727472f5196ef35c05dc3eca6b530a9b256f
Maheshwari <maheshwarikothagadi@gmail.com>
2026-03-11 22:20:54 +0530

    Sudoku

---
+++ solveSudoku_37.py
@@ -0,0 +1,39 @@
+#37
+def solveSudoku(board):
+    rows = [set() for _ in range(9)]
+    cols = [set() for _ in range(9)]
+    boxes = [set() for _ in range(9)]
+    for r in range(3):
+        for c in range(3):
+            if board[r][c] != ".":
+                num = board[r][c]
+                rows[r].add(num)
+                cols[c].add(num)
+                boxes[ r//3 * 3 + c//3].add(num)
+    
+    def backtrack(r, c):
+        if r == 3:
+            return True
+        if c == 3:
+            return backtrack(r + 1, 0)
+        if board[r][c] != ".":
+            return backtrack(r, c + 1)
+        boxid = r//3 * 3 + c//3
+        for num in "123456789":
+            if num not in rows[r] and num not in cols[c] and num not in boxes[boxid]:
+                board[r][c] = num
+                rows[r].add(num)
+                cols[c].add(num)
+                boxes[boxid].add(num)
+                if backtrack(r, c + 1):
+                    return True
+                board[r][c] = "."
+                rows[r].remove(num)
+                cols[c].remove(num)
+                boxes[boxid].remove(num)
+        return False
+    return backtrack(0, 0)
+
+#main
+board = [["2", ".", "3" ], ["1", ".", "2"], ["3", ".", "1"]]
+print(solveSudoku(board))
