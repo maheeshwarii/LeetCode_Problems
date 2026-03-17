91f36393fda2843b742ff05b768a2b89bfec78e9
Maheshwari <maheshwarikothagadi@gmail.com>
2026-03-17 22:44:03 +0530

    Largest submatrix with rearrangements

---
+++ largestSubmatrix_1727.py
@@ -0,0 +1,15 @@
+def largestSubmatrix(matrix):
+    for i in range(1, len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j]:
+                matrix[i][j] = matrix[i - 1][j] + 1
+    ans = 0
+    for row in matrix:
+        row.sort(reverse=True)
+        for j, v in enumerate(row, 1):
+            ans = max(ans, j * v)
+    return ans
+
+#main
+matrix = [[0,0,1],[1,1,1],[1,0,1]]
+print(largestSubmatrix(matrix))
